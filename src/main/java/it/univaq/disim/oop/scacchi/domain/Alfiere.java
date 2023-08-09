86c74ed0d307e0550d6d303a78a7a680ba508ad8
AndreaLarivera <64405125+AndreaLarivera@users.noreply.github.com>
2023-08-09 16:11:25 +0200

    Modifica della classe Alfiere

--- src/main/java/it/univaq/disim/oop/scacchi/domain/Alfiere.java
+++ src/main/java/it/univaq/disim/oop/scacchi/domain/Alfiere.java
@@ -1,5 +1,59 @@
 package it.univaq.disim.oop.scacchi.domain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import it.univaq.disim.oop.scacchi.controller.ScacchieraController;
+import it.univaq.disim.oop.scacchi.domain.Mossa.Attacco;
+import it.univaq.disim.oop.scacchi.domain.Mossa.Muovi;
+
 public class Alfiere extends Pezzo {
+
+	private final static int[] MOSSE_POSSIBILI  = {-9, -7, 7, 9};
+	
+	Alfiere(int coordinatePezzo, Colore colorePezzo) {
+		super(coordinatePezzo, colorePezzo);
+	}
+
+	@Override
+	public Collection<Mossa> calcolaMosseLegali(Scacchiera scacchiera) {
+		
+		final List<Mossa> possibiliMosse = new ArrayList<Mossa>();
+		
+		for(final int offsetPossibiliCoordinate: MOSSE_POSSIBILI) {
+			int coordinateArrivo = this.coordinatePezzo;	
+			while(ScacchieraController.casellaDisponibile(coordinateArrivo)) {
+				if(isPrimaColonnaEsclusa(coordinateArrivo, offsetPossibiliCoordinate) || isOttavaColonnaEsclusa(coordinateArrivo, offsetPossibiliCoordinate)) {
+					break;
+				}
+				coordinateArrivo += offsetPossibiliCoordinate;
+				if(ScacchieraController.casellaDisponibile(coordinateArrivo)) {
+					final Casella casellaArrivo = scacchiera.getCasella(coordinateArrivo);
+					if(!casellaArrivo.occupata()) {
+						possibiliMosse.add(new Muovi(scacchiera, this, coordinateArrivo));
+					}else {
+						final Pezzo pezzoArrivo = casellaArrivo.getPezzo();
+						final Colore colorePezzo = pezzoArrivo.getColorePezzo();
+						if(this.colorePezzo != colorePezzo) {
+							possibiliMosse.add(new Attacco(scacchiera, this, coordinateArrivo, pezzoArrivo));
+						}
+						break;
+					}
+				}
+			}
+		}
+		
+		return Collections.unmodifiableList(possibiliMosse);
+	}
+	
+	private static boolean isPrimaColonnaEsclusa(final int posizioneAttuale, final int possibilePosizione) {
+		return ScacchieraController.PRIMA_COLONNA[posizioneAttuale] && (possibilePosizione == -9 || possibilePosizione == 7);
+	}
+	
+	private static boolean isOttavaColonnaEsclusa(final int posizioneAttuale, final int possibilePosizione) {
+		return ScacchieraController.OTTAVA_COLONNA[posizioneAttuale] && (possibilePosizione == -7 || possibilePosizione == 9);
+	}
 
 }
