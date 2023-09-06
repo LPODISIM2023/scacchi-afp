e45561f60f0f43344d84cc8b014bfcba06dafffd
Paolo Celani <pcelani03@hotmail.com>
2023-09-06 17:15:13 +0200

    correzioni varie

---
+++ src/main/java/it/univaq/disim/oop/scacchi/gui/ScaccoMatto.java
@@ -0,0 +1,43 @@
+package it.univaq.disim.oop.scacchi.gui;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ScaccoMatto extends JDialog {
+
+	ScaccoMatto(final JFrame frame, final boolean modal) {
+		super(frame, modal);
+		final JPanel myPanel = new JPanel(new GridLayout(0, 1));
+
+		getContentPane().add(myPanel);
+		myPanel.add(new JLabel("Scacco Matto"));
+
+		final JButton okButton = new JButton("OK");
+
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ScaccoMatto.this.setVisible(false);
+			}
+		});
+
+		myPanel.add(okButton);
+
+		setLocationRelativeTo(frame);
+		pack();
+		setVisible(false);
+
+	}
+
+	void promptScaccoMatto() {
+		setVisible(true);
+		repaint();
+	}
+
+}
